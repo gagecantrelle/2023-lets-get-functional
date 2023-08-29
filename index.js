b7688c78a90500912a4abfe16e3879768cdee71b
Gage <gagecantrelle1899@gmail.com>
2023-08-29 15:31:21 -0500

    got 4/10 of the problems completed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 'use strict';
 
 var customers = require('./data/customers.json');
-var _ = require(/* Replace this with the name of your lodown! */);
+var _ = require('underpants-gagecantrelle');
 
 /**
  * 1. Import your lodown module using the require() method,
@@ -21,27 +21,89 @@ var _ = require(/* Replace this with the name of your lodown! */);
  *    IMPORTANT: Make sure you replace <YOUR_GITHUB_FOLDER with your actual github folder name that is in your workspace.
  */
 
-var maleCount = function(array) {
+var maleCount = function (array) {
+  //create a variable that holds the return value of _.filter
+  var arr = _.filter(array, function (obj) {
+    //check if the current object has a gender key that is equal to 'male'
+    if (obj.gender === 'male') {
+      //if so return that object
+      return obj;
+    }
+  })
+  //return the length of arr
+  return arr.length;
+};
+
+var femaleCount = function (array) {
+  //create a variable that holds the return value of _.reduce
+  var arr = _.reduce(array, function (seed, obj, index) {
+    //check if the current object has a gender key that is equal to 'female'
+    if (obj.gender === 'female') {
+      //if so return that seed + 1
+      return seed + 1;
+      //run if false
+    } else {
+      //return seed
+      return seed;
+    }
+
+  }, 0)
+  //return arr
+  return arr;
+};
+
+var oldestCustomer = function (array) {
+
+  var arr = [];
+  var customer = [];
+  for (let i = 0; i < array.length; i++) {
+    customer.push(array[i].name);
+    customer.push(array[i].age);
+    arr.push(customer);
+    customer = [];
+  }
+  arr = arr.sort((a, b) => { return b[1] - a[1] });
+
+  return arr[0][0];
+};
+
+var youngestCustomer = function (array) {
+  var arr = [];
+  var customer = [];
+  for (let i = 0; i < array.length; i++) {
+    customer.push(array[i].name);
+    customer.push(array[i].age);
+    arr.push(customer);
+    customer = [];
+  }
+  arr = arr.sort((a, b) => { return a[1] - b[1] });
+
+  return arr[0][0];
+};
+
+var averageBalance = function (array) {
 
 };
 
-var femaleCount;
+var firstLetterCount = function (array, letter) {
 
-var oldestCustomer;
+};
 
-var youngestCustomer;
+var friendFirstLetterCount = function (array, customers, letter) {
 
-var averageBalance;
+};
 
-var firstLetterCount;
+var friendsCount = function (array, name) {
 
-var friendFirstLetterCount;
+};
 
-var friendsCount;
+var topThreeTags = function (array) {
 
-var topThreeTags;
+};
 
-var genderCount;
+var genderCount = function (array) {
+
+};
 
 //////////////////////////////////////////////////////////////////////
 // DON'T REMOVE THIS CODE ////////////////////////////////////////////
